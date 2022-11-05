734ddf0c5a453e7fbad3f227d0cdd8d333fe76ef
Jinping Wu <jinping.wu@verisilicon.com>
2022-11-05 15:30:39 +0800

    add guessing game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,42 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let mut count = 0;
+
+    //println!("The secret number is: {secret_number}");
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+        count += 1;
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("!!! Please enter number");
+                continue;
+            }
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!\n"),
+            Ordering::Greater => println!("Too big!\n"),
+            Ordering::Equal => {
+                println!("You win! count={count}");
+                break;
+            }
+        }
+    }
+}
